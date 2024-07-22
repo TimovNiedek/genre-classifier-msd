8f6d688c9173f72eca11ad9707edbc7d69ff3e78
Timo van Niedek <timovanniedek@gmail.com>
2024-07-22 12:53:52 +0200

    🧱 Set up docker work pool

--- terraform/data.tf
+++ terraform/data.tf
@@ -2,5 +2,6 @@ data "template_file" "prefect_install" {
   template = file("${path.module}/templates/install_prefect.tpl")
   vars = {
     prefect_service = file("${path.module}/templates/prefect_server.service.tpl")
+    work_pool_service = file("${path.module}/templates/prefect_work_pool.service.tpl")
   }
 }
