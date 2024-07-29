b9afa641616e501cb9ef21296083108992e90b75
Timo van Niedek <timovanniedek@gmail.com>
2024-07-29 20:45:59 +0000

    ✏️ Fix typo in s3 bucket name

--- prefect/blocks/create_s3_buckets.py
+++ prefect/blocks/create_s3_buckets.py
@@ -4,7 +4,7 @@ from prefect_aws import AwsCredentials, S3Bucket
 def create_s3_buckets():
     aws_credentials_block = AwsCredentials.load("aws-creds")
     s3_bucket_block = S3Bucket(
-        bucket_name="million_songs_dataset",
+        bucket_name="million-songs-dataset",
         credentials=aws_credentials_block,
     )
     s3_bucket_block.save(name="million-songs-dataset-s3", overwrite=True)
