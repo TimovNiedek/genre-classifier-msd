534378f605b8018bdbcf109e7cf567ca22e95991
Timo van Niedek <timovanniedek@gmail.com>
2024-08-08 14:39:36 +0000

    🚀 Add calls to create prefect block configs

--- deploy.py
+++ deploy.py
@@ -3,6 +3,9 @@ from prefect.deployments import DeploymentImage
 import pendulum
 from slugify import slugify
 
+from genre_classifier.blocks.create_aws_credentials import create_aws_creds_block
+from genre_classifier.blocks.create_s3_buckets import create_s3_buckets
+
 from genre_classifier.flows.ingest_data.flow import ingest_flow
 from genre_classifier.flows.preprocess.flow import preprocess_flow
 from genre_classifier.flows.split_data.flow import split_data_flow
@@ -13,6 +16,9 @@ VERSION = "v0"
 
 
 if __name__ == "__main__":
+    create_aws_creds_block()
+    create_s3_buckets()
+
     deploy(
         ingest_flow.to_deployment(name=f"genre-classifier-ingest-{VERSION}"),
         preprocess_flow.to_deployment(name=f"genre-classifier-preprocess-{VERSION}"),
