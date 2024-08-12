81af2fbec9da0ba430a480cad8ebcbf017b370b3
Timo van Niedek <timovanniedek@gmail.com>
2024-08-12 09:44:48 +0000

    🚧 Use run count for iterating

--- genre_classifier/flows/predict/flow.py
+++ genre_classifier/flows/predict/flow.py
@@ -1,20 +1,18 @@
 import datetime
 
 from prefect import flow, task
-
-current_date = datetime.date.today()
+from prefect import runtime
 
 
 @task
-def print_current_date():
-    global current_date
-    current_date = current_date + datetime.timedelta(days=1)
+def print_current_date(start_date):
+    current_date = start_date + datetime.timedelta(days=runtime.flow_run.run_count - 1)
     print(current_date)
 
 
 @flow(log_prints=True)
-def predict_flow():
-    print_current_date()
+def predict_flow(start_date=datetime.date.today()):
+    print_current_date(start_date)
 
 
 if __name__ == "__main__":
