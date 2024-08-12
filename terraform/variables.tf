b5022fa328a7f0947f41b6345c65fe9d91059e89
Timo van Niedek <timovanniedek@gmail.com>
2024-08-12 11:29:45 +0000

    🛂 Add aws credentials to prefect instance

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,13 @@ variable "dev_ssh_public_key" {
   description = "Public SSH key to use for the Prefect instance"
   type        = string
 }
+
+variable "aws_access_key_id" {
+  description = "AWS access key ID for authenticating from the prefect instance"
+  type        = string
+}
+
+variable "aws_access_key_secret" {
+  description = "AWS access key secret for authenticating from the prefect instance"
+  type        = string
+}
