d042c54f44d245bdcbe49b06ad08617f792c0d38
Timo van Niedek <timovanniedek@gmail.com>
2024-08-13 13:55:32 +0200

    👷 Prepare a cicd bucket for integration testing

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -4,6 +4,13 @@ resource "aws_s3_bucket" "million_songs_dataset" {
   force_destroy = true
 }
 
+resource "aws_s3_bucket" "million_songs_dataset_cicd" {
+  bucket = "million-songs-dataset-cicd"
+
+  force_destroy = true
+}
+
+
 resource "aws_s3_bucket" "mlflow-artifacts-tvn" {
   bucket = "mlflow-artifacts-tvn"
 
