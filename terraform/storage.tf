66618ae60ff0d52541e58ddd26310d62fef8830f
Timo van Niedek <timovanniedek@gmail.com>
2024-08-14 14:21:00 +0200

    🧱 Add separate bucket with static pages

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -16,3 +16,26 @@ resource "aws_s3_bucket" "mlflow-artifacts-tvn" {
 
   force_destroy = true
 }
+
+resource "aws_s3_bucket" "evidently_static_dashboard_tvn" {
+  bucket = "evidently-static-dashboard-tvn"
+
+  force_destroy = true
+
+}
+
+resource "aws_s3_bucket_website_configuration" "evidently_dashboard_config" {
+  bucket = aws_s3_bucket.evidently_static_dashboard_tvn.id
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket                  = aws_s3_bucket.evidently_static_dashboard_tvn.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
